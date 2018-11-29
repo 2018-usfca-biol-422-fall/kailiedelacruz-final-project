12e78ebd363e64cb3d2e73e5ad16ac35443ceda7
Kailie Dela Cruz <kmdelacruz@dons.usfca.edu>
2018-11-29 11:12:31 -0800

    Add more detailed comments to download_fastq.sh file

--- scripts/download_fastq.sh
+++ scripts/download_fastq.sh
@@ -1,4 +1,9 @@
 # the pipe and tail -n +2 is a handy way to exclude the first line
+# A complete pathname was included that takes the function directly
+# to the downloaded sratoolkit
+# The number 7 is used because this is the column that holds
+# the SRA_numbers. The output of this function is being put into
+# the data/raw_data folder
 for SRA_number in $(cut -f 7 data/metadata/SraRunTable.txt | tail -n +2)
 do
     /Users/kailiedelacruz/Downloads/sratoolkit.2.9.2-mac64/bin/fastq-dump -v $SRA_number -O data/raw_data
